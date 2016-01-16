2d7587550f28741329ce584930afb2ecdc8ead0a
Hyeungshik Jung <hyungshike@gmail.com>
2016-01-17 00:20:17 +0900

    fill missing /\..*rc/

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-require('../stylesheets/style.scss');
+require('../stylesheets/style.scss')
 
 import App from './App.jsx'
 import MakeTimetable from './components/make-timetable.jsx'
@@ -9,7 +9,7 @@ import MyTimetable from './components/my-timetable.jsx'
 import ExportTimetable from './components/export-timetable.jsx'
 
 render((
-    <Router history={browserHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={MakeTimetable} />
       <Route path="my" component={MyTimetable} />
