63ceacb2dfae6796776d820640c85acfc265f0b0
Hyeungshik Jung <hyungshike@gmail.com>
2016-03-28 15:08:47 +0900

    Decorate result table

--- src/components/make_timetable/ResultTable.jsx
+++ src/components/make_timetable/ResultTable.jsx
@@ -8,9 +8,9 @@ class ResultRow extends Component {
         onClick={this.props.handleSelect}
         className={this.props.isSelected ? "info" : ""}
       >
-        <td>{this.props.course_number}</td>
-        <td>{this.props.lecture_number}</td>
-        <td>
+        <td className='col-course-no'>{this.props.course_number}</td>
+        <td className='col-lecture-no'>{this.props.lecture_number}</td>
+        <td className='col-title'>
           {this.props.course_title}
           {this.props.isSelected ?
             <button type="button" className="btn btn-success" aria-label="Left Align" onClick={this.props.handleAdd}>
@@ -19,9 +19,10 @@ class ResultRow extends Component {
             null
           }
         </td>
-        <td>{this.props.department}</td>
-        <td>{this.props.class_time}</td>
-        <td>{this.props.remark}</td>
+        <td className='col-department'>{this.props.department}</td>
+        <td className='col-time'>{this.props.class_time}</td>
+        <td className='col-professor'>{this.props.professor}</td>
+        <td className='col-remark'>{this.props.remark}</td>
       </tr>
     )
   }
@@ -49,37 +50,51 @@ export default class ResultTable extends Component {
     this.props.handleAdd(this.props.data[idx])
   }
 
+  tabs() {
+    return (
+      <ul className='tab-list'>
+        <li className='tab-button active'>검색결과</li>
+        <li className='tab-button'>내 강좌</li>
+      </ul>
+    )
+  }
+
   render() {
-    return(<div className="result-wrapper">
-      <table className="table table-hover resultTable">
-        <thead>
-          <tr>
-            <th>과목번호</th>
-            <th>강좌번호</th>
-            <th>과목이름</th>
-            <th>학과</th>
-            <th>시간</th>
-            <th>비고</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            this.props.isQuerying ?
+    return(
+      <div>
+        {this.tabs()}
+      <div className='result-wrapper'>
+        <table className="table table-hover resultTable">
+          <thead>
             <tr>
-              <td colSpan='6'><Loading type='spin' color='#e3e3e3' /></td>
-            </tr> :
-            null
-          }
-          {this.props.data.map((key, idx) => (
-            <ResultRow {...key}
-              key={key._id}
-              handleSelect={this.handleSelect.bind(this, idx)}
-              handleAdd={this.handleAdd.bind(this, idx)}
-              isSelected={this.state.selectedIdx == idx}
-            />
-          ))}
-        </tbody>
-      </table>
+              <th className='col-course-no'>과목</th>
+              <th className='col-lecture-no'>강좌</th>
+              <th className='col-title'>이름</th>
+              <th className='col-department'>학과</th>
+              <th className='col-time'>시간</th>
+              <th className='col-professor'>교수</th>
+              <th className='col-remark'>비고</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.props.isQuerying ?
+              <tr>
+                <td colSpan='6'><Loading type='spin' color='#e3e3e3' /></td>
+              </tr> :
+              null
+            }
+            {this.props.data.map((key, idx) => (
+              <ResultRow {...key}
+                key={key._id}
+                handleSelect={this.handleSelect.bind(this, idx)}
+                handleAdd={this.handleAdd.bind(this, idx)}
+                isSelected={this.state.selectedIdx == idx}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>)
   }
 }
