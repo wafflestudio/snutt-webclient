c4c5da18f5fa4eaf3b03d052ba6d9c5815492499
Hyeungshik Jung <hyungshike@gmail.com>
2016-05-16 14:33:09 +0900

    Temporary footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,9 +3,22 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
   render() {
     return (
-      <h1>
-        Hello, I'm footer
-      </h1>
+      <div
+        className="container"
+        style={footerStyle}
+      >
+        <img
+          width="170px"
+          height="80px"
+          src="https://raw.githubusercontent.com/wafflestudio/snutt/staging/assets/images/waffle_logo.png"
+        />
+        <h6><b>와플스튜디오 SNUTT 팀</b>에서 개발 및 관리하고 있습니다!</h6>
+        <h6>Source code와 contribution 정보는 <a href="https://github.com/wafflestudio/snutt"><b>SNUTT GitHub</b></a>에서 보실 수 있어요.</h6>
+      </div>
     );
   }
+}
+
+const footerStyle = {
+  textAlign: 'center',
 }
