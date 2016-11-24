eaef3c76d19d5966f4aee5ca00cdc337cd69fb67
Hyeungshik Jung <hyungshike@gmail.com>
2016-11-24 17:54:32 +0900

    Update react hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        loaders: ['react-hot', 'babel'],
+        loaders: ['babel'],
         include: path.join(__dirname, 'src')
       },
       {
