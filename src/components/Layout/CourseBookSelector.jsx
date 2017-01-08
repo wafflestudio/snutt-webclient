2691e9973aa4fd625c6a3607d06150ddc802fc3e
Hyeungshik Jung <hyungshike@gmail.com>
2017-01-08 19:40:30 +0900

    Collapse navbar when clicked outer region
    Thanks to anonymous feedback
    "2016-2 학기 선택하는 드롭다운(?) 메뉴를 클릭해서 선택지들이 펼쳐진 후에 다른 곳을 클릭하면 자동으로 접혔으면 좋겠습니다."

--- src/components/Layout/CourseBookSelector.jsx
+++ src/components/Layout/CourseBookSelector.jsx
@@ -17,39 +17,47 @@ class CourseBookSelector extends Component {
     this.props.dispatch(updateCoursebook())
   }
 
-  handleSelect(selectedSemester) {
+  handleSelect(e, selectedCoursebook) {
     this.setState({ opened: false })
-    this.props.dispatch(changeCoursebook(selectedSemester))
+    this.props.dispatch(changeCoursebook(selectedCoursebook))
   }
+  toggleOpened = () => {this.setState({opened: !this.state.opened})}
+  handleClose = () => {this.setState({opened: false})}
 
   render() {
     const { courseBooks, currentBook } = this.props
 
     if (courseBooks.length == 0)  // When coursebook fetch is not finished
       return <li className='dropdown'></li>
-
-    return (
-      <li className={'dropdown' + (this.state.opened ? ' open' : '')}>
-        <a
-          className='button dropdown-toggle'
-          onClick={()=>this.setState({opened: !this.state.opened})}
+    else {
+      return (
+        <li
+          className={'dropdown' + (this.state.opened ? ' open' : '')}
+          tabIndex={1}
+          onBlur={this.handleClose}
         >
-          {`${currentBook.year}-${idxToString[currentBook.semester]}`}
-          <span className='caret' />
-        </a>
-        <ul className='dropdown-menu'>
-          {courseBooks.map((e, i) => (
-            <li key={i}>
-              <a
-                onClick={()=>this.handleSelect(e)}
-              >
-                {`${e.year}-${idxToString[e.semester]}`}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </li>
-    )
+          <a
+            className='button dropdown-toggle'
+            onClick={this.toggleOpened}
+
+          >
+            {`${currentBook.year}-${idxToString[currentBook.semester]}`}
+            <span className='caret' />
+          </a>
+          <ul className='dropdown-menu'>
+            {courseBooks.map((courseBook, i) => (
+              <li key={i}>
+                <a
+                  onClick={(e)=>this.handleSelect(e, courseBook)}
+                >
+                  {`${courseBook.year}-${idxToString[courseBook.semester]}`}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </li>
+      )
+    }
   }
 }
 
