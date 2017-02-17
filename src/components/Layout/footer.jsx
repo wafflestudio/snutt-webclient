6e893f96e7667165c452f95cadf253a981029a2e
Hyeungshik Jung <hyungshike@gmail.com>
2017-02-18 08:20:16 +0900

    Add terms of service, privacy_policy, and oss notice

--- src/components/Layout/footer.jsx
+++ src/components/Layout/footer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { baseUrl } from '../../samples/sampleKey'
 
 export default class Footer extends Component {
   render() {
@@ -15,6 +16,7 @@ export default class Footer extends Component {
         />
         <h6><b>와플스튜디오 <Link to="/about">SNUTT 팀</Link></b>에서 개발 및 관리하고 있습니다!</h6>
         <h6>Source code와 contribution 정보는 <a href="https://github.com/wafflestudio/snutt"><b>SNUTT GitHub</b></a>에서 보실 수 있어요.</h6>
+        <h6><a href={`${baseUrl}terms_of_service`}>약관</a> 및 <a href={`${baseUrl}privacy_policy`}>개인정보 처리방침</a></h6>
       </div>
     );
   }
