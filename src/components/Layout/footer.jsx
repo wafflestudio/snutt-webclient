e50002fbd1ce397c93af0be9a61f60390cc777a8
Hyeungshik Jung <hyungshike@gmail.com>
2017-03-04 15:44:48 +0900

    Add link to developer info

--- src/components/Layout/footer.jsx
+++ src/components/Layout/footer.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { baseUrl } from '../../samples/sampleKey'
 
+const MEMBER_URL = `${baseUrl}member`
+const TERM_URL = `${baseUrl}terms_of_service`
+const PRIVACY_URL = `${baseUrl}privacy_policy`
+
 export default class Footer extends Component {
   render() {
     return (
@@ -14,9 +18,9 @@ export default class Footer extends Component {
           height="80px"
           src="https://raw.githubusercontent.com/wafflestudio/snutt/staging/assets/images/waffle_logo.png"
         />
-        <h6><b>와플스튜디오 <Link to="/about">SNUTT 팀</Link></b>에서 개발 및 관리하고 있습니다!</h6>
+        <h6>와플스튜디오 <b><a href={MEMBER_URL}>SNUTT 팀</a></b>에서 개발 및 관리하고 있습니다!</h6>
         <h6>Source code와 contribution 정보는 <a href="https://github.com/wafflestudio/snutt"><b>SNUTT GitHub</b></a>에서 보실 수 있어요.</h6>
-        <h6><a href={`${baseUrl}terms_of_service`}>약관</a> 및 <a href={`${baseUrl}privacy_policy`}>개인정보 처리방침</a></h6>
+        <h6><a href={TERM_URL}>약관</a> 및 <a href={PRIVACY_URL}>개인정보 처리방침</a></h6>
       </div>
     );
   }
