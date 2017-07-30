6896dcdfc1fa26fb26f20af1cc0c6f8ba9c6e4cf
Hyeungshik Jung <hyungshike@gmail.com>
2017-07-30 21:38:05 +0900

    Style new search bar

--- src/components/Layout/Header/SearchBar.jsx
+++ src/components/Layout/Header/SearchBar.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
+import SearchIcon from '../../../../assets/ic-search.svg';
+import SearchConditionIcon from '../../../../assets/ic-search-condition.svg';
+
 class SearchBar extends Component {
   render() {
     return (
-      <div id="searchbar-wrapper" />
+      <div id="searchbar-wrapper">
+        <input id="query" />
+        <div id="tools">
+          <SearchIcon id="search" />
+          <SearchConditionIcon />
+        </div>
+      </div>
     );
   }
 }
