5de96f2cba2318f8e9edddc3d970c147601b7f9a
Hyeungshik Jung <hyungshike@gmail.com>
2017-07-30 22:34:01 +0900

    Show last update at placeholder

--- src/components/Layout/App.jsx
+++ src/components/Layout/App.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-// import TopBar from './TopBar.jsx';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { updateCoursebook } from '../../actions/fetchingActions';
 import Header from './Header';
 import Footer from './footer.jsx';
 
-// export default class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <Header />
-//         {this.props.children}
-//         <Footer />
-//       </div>
-//     );
-//   }
-// }
+class App extends Component {
 
-const App = props => (
-  <div>
-    <Header />
-    {props.children}
-    <Footer />
-  </div>
-);
+  componentWillMount() {
+    // Entry point for other loading actions
+    this.props.dispatch(updateCoursebook());
+  }
 
-export default App;
+  render() {
+    return (
+      <div>
+        <Header />
+        {this.props.children}
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default connect()(App);
--- src/components/Layout/Header/SearchBar.jsx
+++ src/components/Layout/Header/SearchBar.jsx
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import SearchIcon from '../../../../assets/ic-search.svg';
 import SearchConditionIcon from '../../../../assets/ic-search-condition.svg';
 
+const formatLastUpdate = (currentBook) => {
+  if (!currentBook) {
+    return '';
+  }
+  const date = new Date(currentBook.updated_at);
+  return `(수강편람 최근 업데이트: ${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()})`;
+};
+
+const mapStateToProps = ({ courseBook }) => ({
+  currentBook: courseBook.get('current'),
+});
+
 class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = { text: '' };
+    this.onTextChange = e => this.setState({ text: e.target.value });
+  }
+
   render() {
     return (
       <div id="searchbar-wrapper">
-        <input id="query" />
+        <form id="query">
+          <input
+            value={this.state.text}
+            placeholder={`원하는 강의를 검색하세요. ${formatLastUpdate(this.props.currentBook)}`}
+            onChange={this.onTextChange}
+          />
+        </form>
         <div id="tools">
           <SearchIcon id="search" />
           <SearchConditionIcon />
@@ -17,4 +42,4 @@ class SearchBar extends Component {
   }
 }
 
-export default SearchBar;
+export default connect(mapStateToProps)(SearchBar);
