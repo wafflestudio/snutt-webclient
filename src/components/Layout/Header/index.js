1e661802b2c7439582e6f79177b7a7d4f2682614
Hyeungshik Jung <hyungshike@gmail.com>
2017-07-30 21:55:03 +0900

    Add links to new header

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,26 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 import CourseSelector from './CourseSelector.jsx';
 import SearchBar from './SearchBar.jsx';
 
 import Logo from '../../../../assets/logo.svg';
 import AlarmIcon from '../../../../assets/ic-alarm.svg';
 
-const Header = () => (
+const mapStateToProps = (state) => {
+  const { user: { loggedIn, id } } = state;
+  return { loggedIn, id };
+};
+
+const LoginStatus = ({ loggedIn, id }) => (
+  loggedIn ?
+    <Link to="/myPage">
+      <div id="profile">{`${id}님`}</div>
+    </Link> :
+    <Link to="/login">
+      <div id="profile">로그인</div>
+    </Link>
+);
+
+const Header = ({ loggedIn, id }) => (
   <div id="header">
     <div id="header-container">
       {/* Brand  */}
-      <Logo id="logo" />
-      <div id="brand">
-        SNUTT
-      </div>
+      <Link to="/">
+        <Logo id="logo" />
+        <div id="brand">
+          SNUTT
+        </div>
+      </Link>
       <CourseSelector />
-      <div id="profile">
-        leejoo317님
-      </div>
+      <LoginStatus loggedIn={loggedIn} id={id} />
       <AlarmIcon id="noti" />
       <SearchBar />
     </div>
   </div>
 );
 
-export default Header;
+export default connect(mapStateToProps)(Header);
