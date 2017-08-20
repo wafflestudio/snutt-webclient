1e6e4f5fc79a7d22b8ff060b6205151665a0dffd
Hyeungshik Jung <hyungshike@gmail.com>
2017-08-20 15:43:12 +0900

    Style department autocomplete

--- src/components/MakeTimetable/Search/DepartmentForm.jsx
+++ src/components/MakeTimetable/Search/DepartmentForm.jsx
@@ -2,66 +2,41 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Autocomplete from 'react-autocomplete';
 import { addQuery, removeQuery } from '../../../actions';
+import DeleteButton from '../../../../assets/btn-delete-normal.svg';
 
 const MAX_DEPARTMENT = 3;
 
-class DeparmentForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      value: '',
-    };
-    this.handleSelect = this.handleSelect.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-  }
-
-  handleSelect(value) {
-    this.props.dispatch(addQuery('department', value));
-    this.setState({ value: '' });
-  }
-
-  handleDelete(value, e) {
-    e.preventDefault();
-    this.props.dispatch(removeQuery('department', value));
-  }
-
-  render() {
-    const { selectedDepartments, departmentTags } = this.props;
-    return (
-      <div>
-        {selectedDepartments.map((val, idx) => (
-          <div
-            key={idx}
-            className="btn btn-info btn-sm tag-selected"
-            onClick={this.handleDelete.bind(this, val)}
-          >
-            {val}
-            <span className="glyphicon glyphicon-remove" />
-          </div>
-        ))}
-        {selectedDepartments.size < MAX_DEPARTMENT ?
-          <Autocomplete
-            value={this.state.value}
-            items={departmentTags}
-            getItemValue={item => item}
-            shouldItemRender={matchStateToTerm}
-            menuStyle={menuStyle}
-            onChange={(event, value) => this.setState({ value })}
-            onSelect={value => this.handleSelect(value)}
-            renderItem={(item, highlighted) => (
-              <div
-                style={itemStyle(highlighted)}
-              >
-                {item}
-              </div>
-            )}
-          /> :
-          null
-        }
-      </div>
-    );
-  }
-}
+const styles = {
+  menuStyle: {
+    zIndex: 999,
+    backgroundColor: 'white',
+    borderRadius: '14px',
+    position: 'absolute',
+    left: 0,
+    top: '26px',
+    overflow: 'auto',
+    width: '300px',
+    height: '220px',
+    border: '1px solid #d5dbe0',
+  },
+  itemStyle: highlighted => ({
+    zIndex: 1001,
+    backgroundColor: (highlighted ? '#f3f7f8' : 'white'),
+    padding: '5px 0 5px 14px',
+    cursor: 'pointer',
+    height: '24px',
+  }),
+  wrapperStyle: opened => ({
+    zIndex: 1000,
+    height: '28px',
+    borderRadius: '14px',
+    border: '1px solid',
+    borderColor: opened ? '#1bd0c9' : '#d5dbe0',
+    display: 'inline-block',
+    position: 'relative',
+    paddingLeft: '15px',
+  }),
+};
 
 function mapStateToProps(state) {
   const { query, tagList } = state;
@@ -70,34 +45,86 @@ function mapStateToProps(state) {
   return { selectedDepartments, departmentTags };
 }
 
-export default connect(mapStateToProps)(DeparmentForm);
-
-function escapeRegexCharacters(str) {
-  return str.replace(/[.*+?^${}()|[\]\\]/g, '');
-}
-
-function like(str, option = { fromFirstChar: false }) {
-  // replace every character(eg. 'c') to '.+c', except for first character
-  const escaped = escapeRegexCharacters(str);
-  let reg = escaped.replace(/(?!^)(.)/g, '.*$1');
-  if (option.fromFirstChar) { reg = `^${reg}`; }
-  return new RegExp(reg);
-}
-
-function matchStateToTerm(state, value) {
-  const reg = like(value);
-  return reg.test(state);
-}
-
-const menuStyle = {
-  zIndex: 999,
-  backgroundColor: 'white',
-  position: 'fixed',
-  overflow: 'auto',
-  height: '200px',
-};
-
-const itemStyle = highlighted => ({
-  zIndex: 1000,
-  backgroundColor: (highlighted ? '#DEE0DA' : 'white'),
+const mapDispatchToProps = dispatch => ({
+  addQuery: value => dispatch(addQuery('department', value)),
+  removeQuery: value => dispatch(removeQuery('department', value)),
 });
+
+class DeparmentForm extends Component {
+  static likeReg(str, option = { fromFirstChar: false }) {
+    // replace every character(eg. 'c') to '.+c', except for first character
+    const escaped = str.replace(/[.*+?^${}()|[\]\\]/g, '');
+    let reg = escaped.replace(/(?!^)(.)/g, '.*$1');
+    if (option.fromFirstChar) { reg = `^${reg}`; }
+    return new RegExp(reg);
+  }
+
+  static matchStateToTerm(state, value) {
+    const reg = DeparmentForm.likeReg(value);
+    return reg.test(state);
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      value: '',
+      menuOpened: false,
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.onMenuChange = isOpen => this.setState({ menuOpened: isOpen });
+  }
+
+  handleSelect(value) {
+    this.props.addQuery(value);
+    this.setState({ value: '' });
+  }
+
+  handleDelete(value, e) {
+    e.preventDefault();
+    this.props.removeQuery(value);
+  }
+
+
+  render() {
+    const { selectedDepartments, departmentTags } = this.props;
+    return (
+      <div className="departments">
+        {selectedDepartments.map(val => (
+          <div
+            key={val}
+            className="tag-selected"
+            onClick={this.handleDelete.bind(this, val)}
+          >
+            <span>{val}</span>
+            <DeleteButton />
+          </div>
+        ))}
+        {selectedDepartments.size < MAX_DEPARTMENT ?
+          <Autocomplete
+            value={this.state.value}
+            items={departmentTags}
+            inputProps={{ placeholder: '학과명 검색' }}
+            getItemValue={item => item}
+            shouldItemRender={DeparmentForm.matchStateToTerm}
+            menuStyle={styles.menuStyle}
+            onChange={(event, value) => this.setState({ value })}
+            onSelect={value => this.handleSelect(value)}
+            onMenuVisibilityChange={this.onMenuChange}
+            renderItem={(item, highlighted) => (
+              <div
+                style={styles.itemStyle(highlighted)}
+              >
+                {item}
+              </div>
+            )}
+            wrapperStyle={styles.wrapperStyle(this.state.menuOpened)}
+          /> :
+          null
+        }
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DeparmentForm);
