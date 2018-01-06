f81536290bf923b9402f944f1895c798eaa4c1ed
Hyeungshik Jung <hyungshike@gmail.com>
2018-01-06 16:30:48 +0900

    Recommend to use native app in mobile devices

--- src/components/Layout/App.jsx
+++ src/components/Layout/App.jsx
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import MobileDetect from 'mobile-detect';
 
 import { updateCoursebook } from '../../actions/fetchingActions';
 import Header from './Header';
 import Footer from './footer.jsx';
+import AppLink from './appLink.jsx';
 
 class App extends Component {
+  constructor() {
+    super();
+    const md = new MobileDetect(window.navigator.userAgent);
+    const isMobile = Boolean(md.mobile());
+    this.state = {
+      showAppLink: isMobile,
+    };
+  }
 
   componentWillMount() {
     // Entry point for other loading actions
     this.props.dispatch(updateCoursebook());
   }
 
+  closeModal = () => this.setState({ showAppLink: false })
+
   render() {
+    const { showAppLink } = this.state;
     return (
       <div>
+        {showAppLink &&
+          <AppLink
+            isOpen={showAppLink}
+            close={this.closeModal}
+          />}
         <Header />
         {this.props.children}
         <Footer />
