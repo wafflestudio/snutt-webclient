df98edd997185e965ab3a9c4eb0d46fabffe98ad
Hyeungshik Jung <hyungshike@gmail.com>
2019-01-26 11:34:14 +0900

    Copy serviceWorker setting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { compose, createStore, combineReducers, applyMiddleware } from 'redux';
+import {
+ compose, createStore, combineReducers, applyMiddleware 
+} from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -15,6 +17,7 @@ import thunk from 'redux-thunk';
 
 import api from './middleware/api';
 import rootReducer from './reducers';
+import * as serviceWorker from './serviceWorker';
 
 import {
   App,
@@ -77,3 +80,8 @@ render(
   </Provider>,
   document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
