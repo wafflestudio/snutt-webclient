10f3cb22730ef1bca3a6dd41d68b28c4c474a2e8
Hyeungshik Jung <hyungshike@gmail.com>
2019-01-26 23:23:06 +0900

    Weed out radium and decorator

--- src/components/MakeTimetable/CourseEditor/ColoredBlock.jsx
+++ src/components/MakeTimetable/CourseEditor/ColoredBlock.jsx
@@ -1,34 +1,64 @@
 import React, { Component } from 'react';
-import Radium from 'radium'; // To use pseudoclass CSS inside JavaScript
+// import Radium from 'radium'; // To use pseudoclass CSS inside JavaScript
 import Color from 'color';
+import styled from 'styled-components';
 
-@Radium
+const ColoredDiv = styled.div`
+  backgroundColor: ${props => (props.isSelected ? props.bg : 'white')};
+  borderedColor: ${props => props.bg};
+  color: ${props => (props.isSelected ? props.fg : props.bg)};
+  :hover: {
+    backgroundColor: ${props => props.hoverColor}
+    color: ${props => props.bg}
+  }
+`;
+
+// @Radium
 class ColoredBlock extends Component {
-  handleClick = (e) => {
+  handleClick = e => {
     e.preventDefault();
     this.props.handleColorClick(this.props.color);
-  }
+  };
 
   render() {
-    const { color, isSelected, name } = this.props;
-    const style = {
-      backgroundColor: isSelected ? color.bg : 'white',
-      borderColor: color.bg,
-      color: isSelected ? color.fg : color.bg,
-      ':hover': {
-        backgroundColor: Color(color.bg).fade(0.8).rgb().toString(),
-        color: color.bg,
-      },
-    };
+    const {
+      color: { fg, bg },
+      isSelected,
+      name,
+    } = this.props;
+    // const style = {
+    //   backgroundColor: isSelected ? color.bg : 'white',
+    //   borderColor: color.bg,
+    //   color: isSelected ? color.fg : color.bg,
+    //   ':hover': {
+    //     backgroundColor: Color(color.bg)
+    //       .fade(0.8)
+    //       .rgb()
+    //       .toString(),
+    //     color: color.bg,
+    //   },
+    // };
+    const hoverColor = Color(bg)
+      .fade(0.8)
+      .rgb()
+      .toString();
     return (
-      <div
+      <ColoredDiv
+        isSelected={isSelected}
+        fg={fg}
+        bg={bg}
+        hoverColor={hoverColor}
         className="color-circle"
         onClick={this.handleClick}
-        style={style}
       >
         <span>{name}</span>
-      </div>
+      </ColoredDiv>
     );
+    // return (
+    //   <div className="color-circle" onClick={this.handleClick} style={style}>
+    //     <span>{name}</span>
+    //   </div>
+    // );
   }
 }
 
